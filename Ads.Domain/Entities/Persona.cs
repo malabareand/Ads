877c765a1251f90cfbd4b7ad24ac4fff651b6fee
Carlos Solar <malabareand@gmail.com>
2013-11-29 01:49:46 -0300

    entidades de modelo

---
+++ Ads.Domain/Entities/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.Domain.Entidades
+{
+    public class Persona
+    {
+        public string Rut { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public Sexo Sexo { get; set; }
+    }
+}
---
+++ Ads.Domain/Entities/Sexo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.Domain.Entidades
+{
+    public enum Sexo
+    {
+        Femenino = 1,
+        Masculino = 2
+    }
+}
