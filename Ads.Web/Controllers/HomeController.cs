899877a68e6a11a54a3ab93d590740560a331cff
Carlos Solar <malabareand@gmail.com>
2013-11-29 01:53:44 -0300

    controller y view

--- Ads.Web/Controllers/HomeController.cs
+++ Ads.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ads.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,12 @@ namespace Ads.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult Saludar()
+        {
+            return View(new PersonaViewModel());
         }
     }
 }
