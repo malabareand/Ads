bff9ba36fc009ff8c7fae38da607a1f610fa7fa3
Carlos Solar <malabareand@gmail.com>
2013-11-29 01:51:18 -0300

    definicion de interfaz con acciones e implementacion

---
+++ Ads.Domain/IPersonaActions.cs
@@ -0,0 +1,17 @@
+﻿using Ads.Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.Domain
+{
+    public interface IPersonaActions
+    {
+        IEnumerable<Persona> GetAllPersona();
+        IEnumerable<Persona> GetPersonasByPais(string idPais);
+        Persona CreatePersona(string rut, string nombre, string apellido, DateTime fechaNacimiento, Sexo sexo);
+        bool DeletePersona(string id);
+    }
+}
---
+++ Ads.Repository/PersonaActions.cs
@@ -0,0 +1,38 @@
+﻿using Ads.Domain;
+using Ads.Domain.Entidades;
+using Ads.Repository.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ads.Repository
+{
+    public class PersonaActions : IPersonaActions
+    {
+        /***
+         * TODO la idea a futuro es usar IoC,
+         * para q podamos activar el uso de una bd o simplemente utilizar objetos mock
+         ***/
+        public IEnumerable<Persona> GetAllPersona()
+        {
+            return new PersonaMocks().GetPersonas();
+        }
+
+        public IEnumerable<Persona> GetPersonasByPais(string idPais)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Persona CreatePersona(string rut, string nombre, string apellido, DateTime fechaNacimiento, Domain.Entidades.Sexo sexo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeletePersona(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
